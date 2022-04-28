131f1fd237b3060886028dd609e337c668f7dcee
lucas-bertoni <lbertoni1227@gmail.com>
2022-04-27 20:37:05 -0400

    Starting database

--- server/database.js
+++ server/database.js
@@ -1,8 +1,42 @@
 import 'dotenv/config';
 import pg from 'pg';
 
-class Database {
-  constructor () {
-    
+const { Pool } = pg;
+
+export default class Database {
+  constructor (dburl) {
+    this.dburl = dburl;
+  }
+
+  async connect() {
+    this.pool = new Pool({
+      connectionString: this.dburl,
+      ssl: { rejectUnauthorized: false },
+    });
+
+    this.client = await this.pool.connect();
+    console.log("Database Connected!");
+
+    await this.init();
+  }
+  
+  async init() {
+    const queryText = `
+    CREATE TABLE users (
+      id int(11) NOT NULL AUTO_INCREMENT,
+      email varchar(100) NOT NULL,
+      password varchar(100) NOT NULL,
+      first_name varchar(100) NOT NULL,
+      last_name varchar(100) NOT NULL,
+      PRIMARY KEY (id),
+      UNIQUE KEY email_UNIQUE (email),
+      UNIQUE KEY id_UNIQUE (id)
+    )`;
+
+    const result = await this.client.query(queryText);
+  }
+
+  async close() {
+    await this.client.release();
   }
 }
