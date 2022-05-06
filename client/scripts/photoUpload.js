762d64431bf55da00f0229c9b7e18a82e5fd5acf
Yuri Kim <yuri7kim@gmail.com>
2022-05-06 18:21:26 -0400

    basic set up for uploading img

--- client/scripts/photoUpload.js
+++ client/scripts/photoUpload.js
@@ -1,4 +1,4 @@
-import { createPost, getNumPosts } from './crud.js';
+import { createPost, getNumPosts, uploadPP, downloadPP } from './crud.js';
 
 const session_info = {
     user_id: 1
@@ -10,7 +10,9 @@ submit.addEventListener("click", async () => {
   const post_id = parseInt(post_info.count) + 1;
   
   const user_id = session_info.user_id;
-  const picture_path           = 'puppyPhoto.jpg';
+  const picture_path           = await uploadPhoto();
+  console.log(picture_path);
+  /*
   const description       = document.getElementById("photoDescription").value;
   const tag        = document.getElementById("tag").value;
 
@@ -22,24 +24,23 @@ submit.addEventListener("click", async () => {
     alert("There was an error creating your post");
     location.reload();
   }
+  */
 });
 
-/*
-// async function?
-function uploadPhoto(){
-  const newpp = document.getElementById("newpp").files[0];
-  if (newpp) {
-    const upload_res = await crud.uploadPP(newpp);
-    if (!upload_res.status === 200 || !uploadRes.ok) {
+
+// upload a photo
+async function uploadPhoto(){
+  const formFileLg = document.getElementById("formFileLg").files[0];
+  if (formFileLg) {
+    const upload_res = await uploadPP(formFileLg);
+    if (!upload_res.status !== 200 || !upload_res.ok) {
       alert("There was an error uploading your new profile picture");
     } else {
       const newpp_path = (await upload_res.json()).newpp_path;
-      const downloadBlob = await crud.downloadPP(newpp_path);
+      const downloadBlob = await downloadPP(newpp_path);
       const imgURL = URL.createObjectURL(downloadBlob);
-      document.getElementById('profile-picture-editable').src = imgURL;
-      document.getElementById("bfpp").click();
       path_to_pp = newpp_path.slice(newpp_path.indexOf('/2'));
+      return imgURL;
     }
   }
 }
-*/
