58cf7ceef7b833f17574d880ee3b623d8b8b3912
lucas-bertoni <lbertoni1227@gmail.com>
2022-05-07 13:46:25 -0400

    error fixed

--- server/auth.js
+++ server/auth.js
@@ -1,5 +1,5 @@
-// import passport from 'passport';
-// import passportLocal from 'passport-local';
+import passport from 'passport';
+import passportLocal from 'passport-local';
 
 const { Strategy } = passportLocal;
 
