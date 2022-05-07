f18aa1e548e06cfb48e8d74ed5b5b53c2cc3d57d
zeldagirl113 <etorok@umass.edu>
2022-05-07 16:27:27 -0400

    fixing spacing in home.js

--- client/scripts/home.js
+++ client/scripts/home.js
@@ -3,20 +3,11 @@ import * as crud from './crud.js';
 let feed_photos_path = [];
 let feed_photos_scr = [];
 
-/*  "./img/posts/bird1.jpg",
-  "./img/posts/bird2.jpg",
-  "./img/posts/dog1.jpeg",
-  "./img/posts/dog2.webp",
-  "./img/posts/cat1.jpg",
-  "./img/posts/cat2.jpg",
-  "./img/posts/bird1.jpg",
-  "./img/posts/bird2.jpg",*/
-
 window.onload = async function () {
-  //addFeedPhoto();
   getPostFeedPath();
   displayFeed();
 }
+
 const tagFilter = document.getElementById("drop-button");
 tagFilter.addEventListener('change',getPostFeedPath);
 
@@ -24,22 +15,24 @@ async function getPostFeedPath() {
   feed_photos_path = [];
   const tagFilter = document.getElementById("drop-button");
   const results = await crud.getFeed(tagFilter.value);
-  for(let i = 0; i < Object.keys(results).length; ++i){
+
+  for (let i = 0; i < Object.keys(results).length; ++i) {
     feed_photos_path.push([results[i].post_id,results[i].picture_path]);
   }
+
   getPostFeedImages();
 }
 
 async function getPostFeedImages() {
   feed_photos_scr = [];
-  for (let i = 0; i <feed_photos_path.length; ++i){
-    try{
+  for (let i = 0; i <feed_photos_path.length; ++i) {
+    try {
       const downloadPostBlob = await crud.downloadPhoto(feed_photos_path[i][1]);
-      if(downloadPostBlob !== undefined){
+      if (downloadPostBlob !== undefined) {
         const imgURL = URL.createObjectURL(downloadPostBlob);
         feed_photos_scr.push([imgURL,feed_photos_path[i][0]]);
       }
-    } catch (err){
+    } catch (err) {
       console.log(err.message);
     }
   }
@@ -50,14 +43,15 @@ async function getPostFeedImages() {
 
 async function displayFeed() {
   const posts = document.getElementById("posts");
-  while(posts.firstChild){
+  while (posts.firstChild) {
     posts.removeChild(posts.firstChild);
   }
+  
   for (let i = 0; i < Math.ceil(feed_photos_scr.length/2); ++i) {
     const row = document.createElement("div");
     row.classList.add("post-img-row");
     for (let j = 0; j < 2; ++j) {
-      if((2*i + j) < feed_photos_scr.length){
+      if ((2*i + j) < feed_photos_scr.length) {
         const centering = document.createElement("div");
         centering.classList.add("d-flex");
         centering.classList.add("justify-content-center");
@@ -65,8 +59,8 @@ async function displayFeed() {
         photo.classList.add("img-fluid");
         photo.classList.add("rounded");
         photo.classList.add("shadow-sm");
-        photo.id = feed_photos_scr[2*i + j][1];
 
+        photo.id = feed_photos_scr[2*i + j][1];
         photo.src = feed_photos_scr[2*i + j][0];
         
         photo.addEventListener("click", redirected);
